cb15ca940b2e3d8b9499ae01225c085bd6c58a1a
kawingtam <157887159+kawingtam@users.noreply.github.com>
2025-03-18 21:03:15 -0700

    lessons-hw from Mar 17, 2025

--- Basics/traffic_lights.py
+++ Basics/traffic_lights.py
@@ -6,3 +6,14 @@ given input greater than or equal to 1
 example:
 user = 5
 print red, yellow, green, red, yellow"""
+
+a = int(input("Enter a number: "))
+
+for i in range (1,a+1):
+    if i % 3 == 1:
+        print("red")
+    elif i % 3 == 2:
+        print("yellow")
+    elif i % 3 == 0:
+        print("green")
+
