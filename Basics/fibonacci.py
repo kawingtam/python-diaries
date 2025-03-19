23ed5e7dbb8f16c988b49e7492680fe35fc8ec0f
kawingtam <157887159+kawingtam@users.noreply.github.com>
2025-03-19 06:20:43 +0000

    lesson-hw from Mar 17, 2025

--- Basics/fibonacci.py
+++ Basics/fibonacci.py
@@ -6,11 +6,29 @@ user input length of the sequence = 3
 output = 0,1,1"""
 
 user = int(input("Enter a number :"))
-first = 0
-second = 1
-fibonacci = 0
+# first = 0
+# second = 1
+# fibonacci = 1 
+#
 
-for i in range (0,user+1):
+if user == 1:
+    print(0)
+if user == 2:
+    print(0)
+    print(1)
+if user > 2:
+    print(0)
+    print(1)
+    first = 0
+    second = 1
+    for i in range (3, user+1):
+        fibonacci = first + second
+        print(fibonacci)
+        first = second
+        second = fibonacci
+        # fibonacci = first + second
+        
+        
 
 
 
