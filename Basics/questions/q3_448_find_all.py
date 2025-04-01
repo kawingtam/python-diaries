e9df2be64447ffdad38ed43bdf3d720441817ca8
kawingtam <157887159+kawingtam@users.noreply.github.com>
2025-04-01 19:41:53 +0000

    April 1, 2025

---
+++ Basics/questions/q3_448_find_all.py
@@ -0,0 +1,12 @@
+"""April 1, 2025
+
+448. Find All Numbers Disappeared in an Array
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+
+"""
+
+a = input("Enter numbers separated by commas: ")
+
+# Convert input to a list of integers
+a = list(map(int, a.split(",")))
