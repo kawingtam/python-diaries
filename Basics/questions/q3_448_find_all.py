173eea7ed4b27c1463b78c51198f56b9946d633f
kawingtam <157887159+kawingtam@users.noreply.github.com>
2025-04-01 21:42:59 +0000

    April 1, 2025

--- Basics/questions/q3_448_find_all.py
+++ Basics/questions/q3_448_find_all.py
@@ -10,3 +10,15 @@ a = input("Enter numbers separated by commas: ")
 
 # Convert input to a list of integers
 a = list(map(int, a.split(",")))
+
+seen = []
+missing_num = []
+
+for i in a:
+    if i in a:
+        seen.append(i)
+    else:
+        missing_num.append(i)
+
+missing_num.sort()
+print(missing_num)
