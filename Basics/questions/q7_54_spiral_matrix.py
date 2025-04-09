b56fbbc4315d8548b7a57d1d4e9576c34dacf87c
kawingtam <157887159+kawingtam@users.noreply.github.com>
2025-04-09 18:25:19 +0000

    April 9, 2025

--- Basics/questions/q7_54_spiral_matrix.py
+++ Basics/questions/q7_54_spiral_matrix.py
@@ -8,14 +8,21 @@ return all elements of the matrix in spiral order.
 Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 Output: [1,2,3,6,9,8,7,4,5]
 """
+import math
 nums = input("Enter numbers separated by commas: ")
 nums = list(map(int, nums.split(",")))
 
 #create list within list
 row = []
+len_of_outer_matrix= len(nums)
+len_of_inner_matrix=len(x) for x in nums[0]
+print(len_of_inner_matrix)
+
 for i in range(0,len(nums),3):
     x=nums[i]
     y=nums[i+1]
     z=nums[i+2]
     row.append([x,y,z])
 print(row)
+
+result = []
