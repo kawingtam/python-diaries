5aac8af9a5882388cc8c8310e10cd512bb212a5e
kawingtam <157887159+kawingtam@users.noreply.github.com>
2025-04-09 18:06:48 +0000

    April 9, 2025

---
+++ Basics/questions/q7_54_spiral_matrix.py
@@ -0,0 +1,21 @@
+"""April 9, 2025
+
+54. Spiral Matrix
+
+Given an m x n matrix, 
+return all elements of the matrix in spiral order.
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+"""
+nums = input("Enter numbers separated by commas: ")
+nums = list(map(int, nums.split(",")))
+
+#create list within list
+row = []
+for i in range(0,len(nums),3):
+    x=nums[i]
+    y=nums[i+1]
+    z=nums[i+2]
+    row.append([x,y,z])
+print(row)
