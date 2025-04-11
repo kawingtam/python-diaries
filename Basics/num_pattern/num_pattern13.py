b36c56386500148e03bde5a56f39878ce1bf5fcf
kawingtam <157887159+kawingtam@users.noreply.github.com>
2025-04-11 06:56:28 +0000

    April 10, 2025

--- Basics/num_pattern/num_pattern13.py
+++ Basics/num_pattern/num_pattern13.py
@@ -10,35 +10,58 @@ prints:
 14  23  22  21  8
 13  12  11  10  9
 
-Accessing elements using indexing
-matrix[row, column]
-
-Update matrix row
-matrix[1, :] = [11, 12, 13]
-
-Update matrix column
-matrix[:, 2] = [14, 15, 16]
 """
 
-
 a = int(input("Enter a number: "))
-matrix = [[0]*a for i in range (a)]
-#Right, Down, Left, Up
-directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+a_square = a**2
 
-# Start by moving "Right"
-current_direction = 0  
+result=[]
+#setting row and column
+row = 0
+column = 0
 
-# Starting position
-row, col = 0, 0  
+#setting top, bottom, left, right
+top, bottom = 0, a-1
+left, right = 0, a-1
 
-for i in range(a):
-    row += directions[current_direction][0]
-    print(row)
-    col += directions[current_direction][1]
-    print(col)
-    print() 
+for i in range (a_square+1):
+    while top <= bottom and left <= right:
+        #going right
+        for i in range(left,right+1):
+            result.append(i)
+        top +=1
 
-"""    if i % 3 == 2:  
-        # Cycle through 0 → 1 → 2 → 3 → 0
-        current_direction = (current_direction + 1) % 4  """
+        #going down
+        for i in range(top,bottom+1):
+            result.append(i)
+        bottom+=1
+
+print(result)
+
+
+"""
+# goes in spiral
+while top <= bottom and left <= right:
+    #going right
+    for column in range(left,right+1):
+        result.append(matrix[top][column])
+    top +=1
+
+    #going down
+    for row in range (top,bottom+1):
+        result.append(matrix[row][right])
+    right -=1
+
+    #going left
+    if top <= bottom:
+        for column in range(right,left-1,-1):
+            result.append(matrix[bottom][column])
+        bottom -=1
+
+    #going up
+    if left <= right:
+        for row in range (bottom,top-1,-1):
+            result.append(matrix[row][left])
+        left+=1
+"""
+
