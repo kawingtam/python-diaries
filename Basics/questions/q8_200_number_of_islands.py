dbf4d0885dd99248b932964e0445bba71c608a42
kawingtam <157887159+kawingtam@users.noreply.github.com>
2025-04-11 21:22:25 +0000

    April 11, 2025

---
+++ Basics/questions/q8_200_number_of_islands.py
@@ -0,0 +1,17 @@
+"""April 11, 2025
+
+54. Spiral Matrix
+
+Given an m x n 2D binary grid
+'1's (land) and '0's (water)
+return the number of islands.
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+"""
+
