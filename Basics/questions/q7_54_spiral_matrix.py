5cc10fdc0546523dcf3b24743b28a88b804f7a74
kawingtam <157887159+kawingtam@users.noreply.github.com>
2025-04-11 06:45:29 +0000

    April 10, 2025

--- Basics/questions/q7_54_spiral_matrix.py
+++ Basics/questions/q7_54_spiral_matrix.py
@@ -17,8 +17,47 @@ while True:
     matrix.append(nums)
 print(matrix)
 
+
+
+
 #get to know the mxn
-m = len(matrix[0])
-n = len(matrix)
-print(m)
-print(n)
+m = len(matrix[0]) #column
+n = len(matrix) #row
+"""print("m = ",m)
+print("n = ",n)"""
+
+result=[]
+#setting row and column
+row = 0
+column = 0
+
+#setting top, bottom, left, right
+top, bottom = 0, len(matrix)-1
+left, right = 0, len(matrix[0])-1
+
+# goes in spiral
+while top <= bottom and left <= right:
+    #going right
+    for column in range(left,right+1):
+        result.append(matrix[top][column])
+    top +=1
+
+    #going down
+    for row in range (top,bottom+1):
+        result.append(matrix[row][right])
+    right -=1
+
+    #going left
+    if top <= bottom:
+        for column in range(right,left-1,-1):
+            result.append(matrix[bottom][column])
+        bottom -=1
+
+    #going up
+    if left <= right:
+        for row in range (bottom,top-1,-1):
+            result.append(matrix[row][left])
+        left+=1
+
+
+print(result)
