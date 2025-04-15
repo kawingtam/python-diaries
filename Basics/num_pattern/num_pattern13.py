bd32a2fd4cdea0217fd3cecde8d1ffdc1abd6c18
kawingtam <157887159+kawingtam@users.noreply.github.com>
2025-04-15 01:26:28 +0000

    April 14, 2025

--- Basics/num_pattern/num_pattern13.py
+++ Basics/num_pattern/num_pattern13.py
@@ -16,69 +16,15 @@ a = int(input("Enter a number: "))
 a_square = a**2
 
 result=[]
-<<<<<<< HEAD
-=======
-result = [[0] * a for num in range(a)]
->>>>>>> e5a847b000f78f15598cc0723da5d070fcb8e2f2
 
-<<<<<<< HEAD
 row, column = a,a
-=======
->>>>>>> e5a847b000f78f15598cc0723da5d070fcb8e2f2
 
-<<<<<<< HEAD
 top = 0
 bottom = row - 1
 left = 0
 right = column - 1
-=======
-row = a
-column = a
->>>>>>> e5a847b000f78f15598cc0723da5d070fcb8e2f2
-
-<<<<<<< HEAD
-=======
-top = 0
-bottom = row - 1
-left = 0
-right = column - 1
->>>>>>> e5a847b000f78f15598cc0723da5d070fcb8e2f2
-
-<<<<<<< HEAD
-=======
-count = 1
-
-while top <= bottom and left <= right:
-    # go right
-    for i in range (left, right+1):
-        result[top][i] = count
-        count += 1
-    top +=1
-    
-    # go down
-    for j in range (top, bottom+1):
-        result[j][right]=count
-        count+=1
-    right -=1
-
-    # go left
-    if top <= bottom:
-        for k in range(right,left-1,-1):
-            result[bottom][k]=count
-            count+=1
-        bottom -=1
-
-    # up
-    if left <= right:
-        for l in range (bottom,top-1,-1):
-            result[l][left]=count
-            count +=1
-        left += 1
-    
-print(result)
 
 
->>>>>>> e5a847b000f78f15598cc0723da5d070fcb8e2f2
 """
 # goes in spiral
 while top <= bottom and left <= right:
